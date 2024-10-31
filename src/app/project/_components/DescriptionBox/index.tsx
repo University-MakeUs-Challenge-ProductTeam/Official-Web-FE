419264b0e4e9bbf9a3fd0cb33c1f76251e198899
daindaind <cdin23@naver.com>
2024-10-31 17:15:01 +0900

    style: 프로젝트 설명 줄바꿈 스타일 수정

--- src/app/project/_components/DescriptionBox/index.tsx
+++ src/app/project/_components/DescriptionBox/index.tsx
@@ -8,11 +8,11 @@ interface IDescriptionBoxProps {
 
 function DescriptionBox({ description }: IDescriptionBoxProps) {
   return (
-    <div className="flex-3 flex w-full flex-col gap-5 rounded-xl border border-solid border-[#3A3A3A] bg-[#1B1B1B] p-8">
+    <div className="flex w-full flex-col gap-5 rounded-xl border border-solid border-[#3A3A3A] bg-[#1B1B1B] p-8">
       <Typography size="title-sm" className="text-[#9D9D9D]">
         프로젝트 설명
       </Typography>
-      <Typography size="text-lg" className="flex-1 text-[#CFCFCF]">
+      <Typography size="text-lg" className="flex-1 flex-wrap whitespace-pre-wrap text-[#CFCFCF]">
         {description}
       </Typography>
     </div>

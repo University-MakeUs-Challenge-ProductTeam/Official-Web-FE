942d2e9bc2af1be386338e92f9425d9c2dcf8c3e
daindaind <cdin23@naver.com>
2024-11-01 16:30:36 +0900

    feat: 모집 안내 페이지 문의하러 가기 구현

--- src/app/recruitment/page.tsx
+++ src/app/recruitment/page.tsx
@@ -2,6 +2,7 @@ import ActivityBox from './_components/ActivityBox';
 import FaqAccordions from './_components/FaqAccordions';
 
 import Container from '@/shared/components/Container';
+import Typography from '@/shared/components/Typography';
 
 function RecruitmentPage() {
   return (
@@ -10,6 +11,17 @@ function RecruitmentPage() {
       <div className="flex flex-col">
         <h1 className="text-title-smd text-[#ECECEC]">FAQ</h1>
         <FaqAccordions />
+      </div>
+      <div className="mt-20 flex flex-row flex-wrap items-center justify-between gap-5">
+        <Typography as="h1" size="title-smd" className="mr-5 text-[#ECECEC]">
+          UMC에 대해
+          <br />더 궁금한 챌린저라면?
+        </Typography>
+        <button type="button" className="h-[50px] rounded-[100px] border-2 border-solid border-main-green bg-[#1F1F1F] px-[24px]">
+          <Typography size="title-sm" color="main-green" className="font-bold">
+            문의하러 가기
+          </Typography>
+        </button>
       </div>
     </Container>
   );

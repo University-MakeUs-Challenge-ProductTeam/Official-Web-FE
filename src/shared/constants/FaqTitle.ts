c530622519362d5c99af9ab1174e4ff157b6c029
daindaind <cdin23@naver.com>
2024-11-02 20:52:46 +0900

    style: Faq 내용 추가 적용

--- src/shared/constants/FaqTitle.ts
+++
@@ -1,7 +0,0 @@
-export const FAQ_TITLE = [
-  { id: 1, question: 'Q. 한 가지의 파트만 지원 가능한가요?', answer: '' },
-  { id: 2, question: 'Q. 추가 모집 기간에 지원하게 되면 불이익이 있나요?', answer: '' },
-  { id: 3, question: 'Q. 스터디만 참여하고, 프로젝트는 참여하지 않을 수 있나요?', answer: '' },
-  { id: 4, question: 'Q. 다른 IT 동아리와 병행해도 되나요?', answer: '' },
-  { id: 5, question: 'Q. OB인 경우, 혜택이 있나요?', answer: '' },
-];

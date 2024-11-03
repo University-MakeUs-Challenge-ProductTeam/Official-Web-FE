23c3c3a042e3d7b4fd0d12bc3738ce4be5d0e60c
김용민 <dydals3440@gmail.com>
2024-11-03 18:44:11 +0900

    feat: Common Input 컴포넌트 제작, 후원신청 폼 페이지 제작

---
+++ src/app/sponser/_components/Form/index.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import type { FieldValues, SubmitHandler } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
+
+import Button from '@/shared/components/Button';
+import Input from '@/shared/components/Input';
+
+function Form() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>();
+
+  const onSubmit: SubmitHandler<FieldValues> = (data: FieldValues, event) => {
+    event?.preventDefault();
+  };
+
+  return (
+    <form className="flex flex-col items-end gap-8" onSubmit={handleSubmit(onSubmit)}>
+      <Input id="applicationName" label="성함" register={register} errors={errors} required />
+      <Input id="contactInfo" label="연락처" register={register} errors={errors} required />
+      <Input id="email" label="이메일" type="email" register={register} errors={errors} required />
+      <Input id="organizationName" label="기관명" register={register} errors={errors} required />
+      <Input id="logoImage" label="기관로고" register={register} errors={errors} required />
+      <Input id="description" label="기관설명" register={register} errors={errors} required />
+      <Input id="link" label="기관링크" register={register} errors={errors} required />
+      <div className="my-10 w-[160px]">
+        <Button type="submit" variant="outline">
+          제출하기
+        </Button>
+      </div>
+    </form>
+  );
+}
+
+export default Form;
--- src/app/sponser/page.tsx
+++ src/app/sponser/page.tsx
@@ -1,7 +1,19 @@
+import Form from '@/app/sponser/_components/Form';
 import Container from '@/shared/components/Container';
+import Spacing from '@/shared/components/Spacing';
+import Typography from '@/shared/components/Typography';
 
 function SponserPage() {
-  return <Container>Sponser Page</Container>;
+  return (
+    <Container>
+      <Spacing direction="vertical" size={32} />
+      <Typography size="title-smd" color="main-white">
+        후원신청 폼
+      </Typography>
+      <Spacing direction="vertical" size={32} />
+      <Form />
+    </Container>
+  );
 }
 
 export default SponserPage;

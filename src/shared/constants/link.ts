382fa88602d0ee0dc35c5f9ecf414069290c8530
chaKK <104924817+daindaind@users.noreply.github.com>
2024-11-11 20:54:31 +0900

    feature/#19: navbar, footer 스타일 수정 (#20)
    * style: 전체 레이아웃 배경 색상 변경
    
    * style: footer 스타일 변경 및 링크 연결

--- src/shared/constants/link.ts
+++ src/shared/constants/link.ts
@@ -1,1 +1,4 @@
 export const KAKAO_CHAT_LINK = 'http://pf.kakao.com/_xjqxcln/chat';
+export const DEVELOPER_MAIL = 'dydals3440@gmail.com';
+export const LINK_TREE = 'https://linktr.ee/unimakeuschallenge?utm_source=linktree_profile_share&ltsid=b0163db0-9ba0-4e3d-81fd-707e4efbc675';
+export const INSTAGRAM_LINK = 'https://www.instagram.com/uni_makeus_challenge?igsh=dmQ3dmhmZnZtc3Vi';

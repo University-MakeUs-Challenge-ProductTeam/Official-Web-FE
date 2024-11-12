d1958db9b38068eb69d1655e1b3bba311880ebb8
김용민 <dydals3440@gmail.com>
2024-11-12 23:37:36 +0900

    feature/#23 후원사 작성 서버 API 변경사항 반영 (#24)

--- src/shared/api/sponsor.ts
+++ src/shared/api/sponsor.ts
@@ -1,22 +1,13 @@
 import { axiosInstance } from '@/shared/api/axios-instance';
 import type { TPostSponsorResponse, TSponsorDTO } from '@/shared/types/dtos/sponsor';
 
-export const postSponsor = async ({
-  applicationName,
-  contactInfo,
-  description,
-  email,
-  link,
-  logoImage,
-  organizationName,
-}: TSponsorDTO): Promise<TPostSponsorResponse> => {
+export const postSponsor = async ({ applicationName, contactInfo, description, email, link, organizationName }: TSponsorDTO): Promise<TPostSponsorResponse> => {
   const { data } = await axiosInstance.post<TPostSponsorResponse>(`/api/sponsors/request`, {
     applicationName,
     contactInfo,
     description,
     email,
     link,
-    logoImage,
     organizationName,
   });
 

1cf64be72d572201c80b4b0c7bbbcce0c5dd74ef
김용민 <dydals3440@gmail.com>
2024-11-16 11:41:03 +0900

    feature/#37 추가 요구사항 반영  (#40)
    * feat: 이미지 변경사항 반영
    
    * feat: 이미지 변경사항 반영
    
    * style: 아이디어톤 -> PM 데이 명칭 변경
    
    * fix: activity content 명칭 변경
    
    * fix: CentralEventDTO에 PM_DAY 필드 추가

--- src/shared/types/dtos/academy.ts
+++ src/shared/types/dtos/academy.ts
@@ -1,4 +1,13 @@
 export const EventType = {
+<<<<<<< HEAD
+=======
+  '교내 OT': 'SCHOOL_OT',
+  '교내 해커톤': 'SCHOOL_HACKATHON',
+  '너디너리 데모데이': 'NEORDINARY_DEMO_DAY',
+  'PM 데이': 'PM_DAY',
+  '데모데이': 'UMC_DEMO_DAY',
+  '너디너리 해커톤': 'NEORDINARY_HACKATHON',
+>>>>>>> 4418acd (feature/#37 추가 요구사항 반영  (#40))
   '연합 OT': 'UNION_OT',
   '학교 OT': 'SCHOOL_OT',
   'Core Day': 'CORE_DAY',

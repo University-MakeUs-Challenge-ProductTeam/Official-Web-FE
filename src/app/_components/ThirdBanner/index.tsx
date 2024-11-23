0d94c815b2eea7184b0e8eec39a986cbcf0fa588
김용민 <dydals3440@gmail.com>
2024-11-24 04:00:23 +0900

    deploy v1.0.0 (#51)
    * deploy: V1.0.0 (#25)
    
    * feature/#26 소개 페이지 반응형 스타일 수정 & 프로젝트 데이터 요청 방식 변경 (#27)
    
    * fix: axiosInstance 적용, next.config.mjs rewrites 적용 해제
    
    * style: 소개 페이지 반응형 스타일 수정
    
    * feature/#29: 활동 정보 요청 axiosInstance 적용 (#30)
    
    * fix: 활동 정보 요청 axiosInstance 적용
    
    * fix: 모집안내 활동정보 쿼리 적용
    
    * feature/#31 반응형 디자인 수정 (#32)
    
    * feat: Navbar font 수정
    
    * style: 메인 페이지 반응형 수정
    
    * style: 메인 페이지 반응형 수정
    
    * feat: 추가 요구사항 반영 (#35)
    
    * chore: SchoolData type logoImageUrl 추가 (#39)
    
    * feature/#37 추가 요구사항 반영  (#40)
    
    * feat: 이미지 변경사항 반영
    
    * feat: 이미지 변경사항 반영
    
    * style: 아이디어톤 -> PM 데이 명칭 변경
    
    * fix: activity content 명칭 변경
    
    * fix: CentralEventDTO에 PM_DAY 필드 추가
    
    * feature/#42 추가 요구사항 반영 및 기타 버그 fix (#43)
    
    * feat: 깃허브 URL 추가
    
    * fix: 학교 슬라이더 무한히 움직이게 설정
    
    * style: lint 에러 수정
    
    * fix: activity image url에activity description이 바인 딩 된 상태 해제
    
    * feature/#42 추가 요구사항 반영 및 기타 버그 fix (#45)
    
    * feat: 깃허브 URL 추가
    
    * fix: 학교 슬라이더 무한히 움직이게 설정
    
    * style: lint 에러 수정
    
    * fix: activity image url에activity description이 바인 딩 된 상태 해제
    
    * fix: UMC 활동 소개 순서 변경
    
    * fix: 활동 정보 -> 연학 & 학교 OT 라인 제거
    
    * fix: 모집 일정 운영진 정보 추가
    
    * Feature/#47 학교 슬라이드 로고 추가 & 후원사 페이지 분리 (#48)
    
    * feat: 학교 로고 이미지 추가
    
    * feat: 후원사 페이지 소개 부분 페이지 추가
    
    * feat: 후원사 목록 배너 구현
    
    * style: 후원사 텍스트 사이즈 조정
    
    * fix: DB logo Image URL 대응
    
    * style: console 삭제
    
    * feat: 도전/평등/자신감 이미지 추가
    
    ---------
    
    Co-authored-by: chaKK <104924817+daindaind@users.noreply.github.com>

--- src/app/_components/ThirdBanner/index.tsx
+++ src/app/_components/ThirdBanner/index.tsx
@@ -1,11 +1,13 @@
+import Image from 'next/image';
+
 import Flex from '@/shared/components/Flex';
 import Spacing from '@/shared/components/Spacing';
 import Typography from '@/shared/components/Typography';
 
 const coreValues = [
-  { id: 1, label: '도전' },
-  { id: 2, label: '평등' },
-  { id: 3, label: '자신감' },
+  { id: 1, label: '도전', path: '/images/도전.png' },
+  { id: 2, label: '평등', path: '/images/평등.png' },
+  { id: 3, label: '자신감', path: '/images/자신감.png' },
 ];
 
 function ThirdBanner() {
@@ -26,9 +28,12 @@ function ThirdBanner() {
       <Flex className="flex flex-col gap-10 md:flex-row">
         {coreValues.map((value) => (
           <Flex key={value.id} direction="column" justify="center" align="center">
-            <div className="size-[230px] rounded-full bg-main-white" />
+            <div className="flex size-[230px] flex-col items-center justify-center rounded-full bg-[#D9D9D9]">
+              <Image src={value.path} width={230} height={230} alt={value.label} className="flex items-center justify-center rounded-full" objectFit="true" />
+            </div>
+
             <Spacing direction="vertical" size={20} />
-            <Typography size="text-lg" color="main-white">
+            <Typography size="text-lg" color="main-disable">
               {value.label}
             </Typography>
           </Flex>
